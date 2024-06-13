e770f368e63facf3f19cd437d690287bd070d766
franCarrascoDLM <fran.carrasco@dataleanmakers.es>
2024-06-13 15:48:21 +0200

    comm #65

--- includes/form_1.php
+++ includes/form_1.php
@@ -9,7 +9,7 @@
 	$message = $_POST['message'];
 	
 	// Create Message	
-	$to = 'francelta@gmail.com';
+	$to = 'davidrenovation@yahoo.com';
 	$email_subject = "Message from a Blocs website.";
 	$email_body = "You have received a new message. \n\nName: $name \nEmail: $email \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
